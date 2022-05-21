1ad54b504daa06123c17ae19baa8efdea742c549
Cristian Peredo <crisperedo64gmail.com>
2022-05-21 20:57:31 -0300

    Clase 1 - JSX

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () =>{
+    return <header>
+        <nav>
+            <div className="nav">
+                <h1>MI PROYECTO</h1>
+
+                <ul>
+                    <li>Menu</li>
+                    <li>Productos</li>
+                    <li>Ayuda</li>
+                </ul>        
+            </div>
+        </nav>
+    </header>
+}
+export default Navbar
