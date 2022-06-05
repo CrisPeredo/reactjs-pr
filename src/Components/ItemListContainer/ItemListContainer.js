4792e4a9b03fe61d528a7081e4811ff86a24cb52
Cristian Peredo <crisperedo64gmail.com>
2022-06-05 19:27:55 -0300

    Clase 7 - JSX

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -3,14 +3,5 @@ import Contador from "../Contador/Contador"
 
 
 export const ItemListContainer = ({nombre, precio}) =>{
-
-    return(
-        <section className="producto">
-            <h3 className="title">Productos</h3>
-            
-            <p className="name__product">Producto: {nombre}</p>
-            <p>Precio {precio}</p>
-            <p> <Contador/></p>
-        </section>
-    )
+ 
 }
